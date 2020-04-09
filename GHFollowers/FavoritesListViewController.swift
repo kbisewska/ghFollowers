4aecd350532fa0819a05ff3230fedc990d302229
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-04-09 16:28:44 +0200

    Configure tab bar screen

---
+++ GHFollowers/FavoritesListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListViewController.swift
+//  GHFollowers
+//
+//  Created by Kora on 09/04/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollowers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  GHFollowers
+//
+//  Created by Kora on 09/04/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
