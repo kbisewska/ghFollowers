44fadd4ab4bf8af8e62919e89dcefb0980983b3d
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-04-10 14:06:53 +0200

    Add snapshot test for SearchViewController

---
+++ GHFollowersTests/SearchViewControllerSnapshotTests.swift
@@ -0,0 +1,32 @@
+//
+//  SearchViewControllerSnapshotTests.swift
+//  GHFollowersTests
+//
+//  Created by Kora on 10/04/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import XCTest
+import SnapshotTesting
+
+@testable import GHFollowers
+
+class SearchViewControllerSnapshotTests: XCTestCase {
+    
+    var sut: SearchViewController!
+
+    override func setUp() {
+        super.setUp()
+        sut = SearchViewController()
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func testSearchViewControllerSnapshot() {
+        assertSnapshot(matching: sut, as: .image(on: .iPhoneXr))
+    }
+
+}
