c8f43ae2461e80dbe8f806437ec18ce2de374a03
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-04-10 15:13:05 +0200

    Create Follower and User models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Kora on 10/04/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
