c8141e3e317a102c6ff5d2aec1780f9e888464cb
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-04-15 12:50:33 +0200

    Create a header view for user info screen

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Kora on 15/04/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
