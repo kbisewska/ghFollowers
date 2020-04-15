1c07ea7af1e0ea25989874888241e3356fee0866
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-04-15 10:39:31 +0200

    Create UserInfoViewController

---
+++ GHFollowers/Screens/UserInfoViewController.swift
@@ -0,0 +1,28 @@
+//
+//  UserInfoViewController.swift
+//  GHFollowers
+//
+//  Created by Kora on 15/04/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoViewController: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissViewController))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        print(username!)
+    }
+    
+    @objc func dismissViewController() {
+        dismiss(animated: true)
+    }
+}
