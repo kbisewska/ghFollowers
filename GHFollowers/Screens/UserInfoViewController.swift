d562a7c601fc8d466b32ec68a58e2bf334e00535
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-04-16 16:08:01 +0200

    Add dateDecodingStrategy to NetworkManager

--- GHFollowers/Screens/UserInfoViewController.swift
+++ GHFollowers/Screens/UserInfoViewController.swift
@@ -65,7 +65,7 @@ class UserInfoViewController: UIViewController {
         self.add(childViewController: GFUserInfoHeaderViewController(user: user), to: self.headerView)
         self.add(childViewController: repoItemViewController, to: self.itemViewOne)
         self.add(childViewController: followerItemViewController, to: self.itemViewTwo)
-        self.dateLabel.text = "GitHub since \(user.createdAt.convertToDisplayFormat())"
+        self.dateLabel.text = "GitHub since \(user.createdAt.convertToMonthYearFormat())"
     }
     
     func layoutUI() {
