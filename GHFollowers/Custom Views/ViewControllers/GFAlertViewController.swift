16c78d7db6729f6798dc983089a6076b24c00f0f
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-04-16 15:55:13 +0200

    Refactor GFAlertViewController

--- GHFollowers/Custom Views/ViewControllers/GFAlertViewController.swift
+++ GHFollowers/Custom Views/ViewControllers/GFAlertViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class GFAlertViewController: UIViewController {
     
-    let containerView = UIView()
+    let containerView = GFAlertContainerView()
     let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
     let messageLabel = GFBodyLabel(textAlignment: .center)
     let actionButton = GFButton(backgroundColor: .systemPink, title: "OK")
@@ -35,7 +35,7 @@ class GFAlertViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
         
         configureContainerView()
         configureTitleLabel()
@@ -44,11 +44,6 @@ class GFAlertViewController: UIViewController {
     }
     
     func configureContainerView() {
-        containerView.backgroundColor = .systemBackground
-        containerView.layer.cornerRadius = 16
-        containerView.layer.borderWidth = 2
-        containerView.layer.borderColor = UIColor.white.cgColor
-        containerView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(containerView)
         
         NSLayoutConstraint.activate([
