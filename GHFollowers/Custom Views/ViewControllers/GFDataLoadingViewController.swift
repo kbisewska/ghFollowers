62b4e49e2ca91d880a37f91aba49e40d0a2ee784
Kornelia Bisewska <bisewskakornelia@gmail.com>
2020-04-16 16:51:31 +0200

    Create GFDataLoadingViewController

---
+++ GHFollowers/Custom Views/ViewControllers/GFDataLoadingViewController.swift
@@ -0,0 +1,48 @@
+//
+//  GFDataLoadingViewController.swift
+//  GHFollowers
+//
+//  Created by Kora on 16/04/2020.
+//  Copyright © 2020 kbisewska. All rights reserved.
+//
+
+import UIKit
+
+class GFDataLoadingViewController: UIViewController {
+    var containerView: UIView!
+
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+        view.addSubview(containerView)
+        
+        UIView.animate(withDuration: 0.25) {
+            self.containerView.alpha = 0.8
+        }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        containerView.addSubview(activityIndicator)
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            self.containerView.removeFromSuperview()
+            self.containerView = nil
+        }
+    }
+    
+    func showEmptyStateView(with message: String, in view: UIView) {
+        let emptyStateView = GFEmptyStateView(message: message)
+        emptyStateView.frame = view.bounds
+        view.addSubview(emptyStateView)
+    }
+}
